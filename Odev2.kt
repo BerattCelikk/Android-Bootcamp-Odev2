8ace6ea090ba17c00a63003c6a902806d221e5bc
BerattCelikk <138959468+BerattCelikk@users.noreply.github.com>
2025-04-25 12:35:12 +0300

    Update Odev2.kt

--- Odev2.kt
+++ Odev2.kt
@@ -25,4 +25,28 @@ class Odev2 {
         }
         return sayac
     }
+    fun soru5(side_number : Int) : Int{
+        var ic_acilar_toplam = (side_number-2)* 180
+        return  ic_acilar_toplam
+    }
+    fun soru6(gun : Int) : Int {
+        var maas = 0
+        val saat = gun * 8
+
+        if (saat <= 160) {
+            maas = saat * 10
+        } else {
+            val mesaiSaat = saat - 160
+            maas = (160 * 10) + (mesaiSaat * 20)
+        }
+
+        return maas
+    }
+    fun soru7(total_gb : Int,) : Int{
+        var ucret = 100
+        if(total_gb >50){
+            ucret = ucret + (total_gb-50)*4
+        }
+        return  ucret
+    }
 }
