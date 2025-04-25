53ef0a645d2e8ad588d5284a59aaebdfbce4f675
BerattCelikk <138959468+BerattCelikk@users.noreply.github.com>
2025-04-25 12:01:02 +0300

    Create Odev2Main.kt

---
+++ Odev2Main.kt
@@ -0,0 +1,15 @@
+package com.beratcelik.kotlindersleri.nesne_tabanli_programlama
+
+fun main() {
+    val soru1 = Odev2()
+    println("Fahrenheit : ${soru1.soru1(30)}")
+
+    val soru2 = Odev2()
+    println("Rectangle : ${soru2.soru2(3,4)}")
+
+    val soru3 = Odev2()
+    println("Factorial : ${soru3.soru3(5)}")
+
+    val soru4 = Odev2()
+    println("Total : ${soru4.soru4("Merhaba")}")
+}
